b2da90576d72897e749c893b968884f1fa371ba2
DrYao <44048541+a371057600@users.noreply.github.com>
2019-05-16 22:22:31 +0800

    重新修改了

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 把
 pipline.py中的数据库改成你的数据库哦,详情操作,请自己去百度找SCRAPY的数据库连接教程,非常简单
 #这里记得改成你自己的数据库,不然你爬起来会不停地出错哦.
+
+2019.5.17号的小修改
+pipline.py中添加了异步下载数据到数据库
+class JianshuTwistedPipeline(object):
+
+并且更改了setting内的
+ITEM_PIPELINES = {
+   # 'jianshu_spider.pipelines.JianshuSpiderPipeline': 300,
+   'jianshu_spider.pipelines.JianshuTwistedPipeline': 300,
+}
+把原本的JianshuSpiderPipeline慢速爬虫，替换成了JianshuTwistedPipeline的异步爬虫
--- jianshu_spider/jianshu_spider/pipelines.py
+++ jianshu_spider/jianshu_spider/pipelines.py
@@ -5,7 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
-
+from twisted.enterprise import adbapi
+from pymysql import cursors
 
 class JianshuSpiderPipeline(object):
     def __init__(self):
@@ -35,3 +36,41 @@ class JianshuSpiderPipeline(object):
                         """
             return self._sql
         return self._sql
+
+
+class JianshuTwistedPipeline(object):
+    def __init__(self):
+        dbparams = {
+            'host': '127.0.0.1',
+            'port': 3306,
+            'user': 'root',
+            'password': '7923161',
+            'database': 'jianshu',
+            'charset': 'utf8',
+            'cursorclass': cursors.DictCursor
+        }
+        self.dbpool = adbapi.ConnectionPool('pymysql',**dbparams)
+        self._sql = None
+
+    @property
+    def sql(self):
+        if not self._sql:
+            self._sql = """
+                            insert into article(id,title,content,author,avatar,pub_time,origin_url,article_id) values(null,%s, %s, %s, %s,%s,%s,%s)
+                            """
+            return self._sql
+        return self._sql
+
+    def process_item(self,item,spider):
+        defer=self.dbpool.runInteraction(self.insert_item,item)
+        defer.addErrback(self.handle_error,item,spider)
+
+    def insert_item(self, cursor, item):
+        cursor.execute(self.sql,(item['title'],item['content'],item['author'],item['avatar'],item['pub_time'],item['origin_url'],item['article_id']))
+
+    def handle_error(self,error,item,spider):
+        print('='*10+"error"+"="*10)
+        print(error)
+        print('='*10+"error"+"="*10)
+
+
